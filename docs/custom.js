0ee351ef35ce465d503228276d8358d62aa2462d
gilvan.moura <gilvan.moura@cnec.br>
2022-01-03 13:42:43 -0300

    custom.js identado

--- docs/custom.js
+++ docs/custom.js
@@ -3,7 +3,7 @@ const pathName = window.location.pathname;
 //const env = require(["../env"]);
 
 function insertAfter(newElement, referenceElement) {
-    referenceElement.parentNode.insertBefore(newElement, referenceElement.nextSibling);
+   referenceElement.parentNode.insertBefore(newElement, referenceElement.nextSibling);
 }
 
 // if(pathName === "/"){
@@ -19,65 +19,65 @@ function insertAfter(newElement, referenceElement) {
 
 // }
 
-function formatMessage(nome, mensagem){
-      var text = "";
-      text +="<h2>";
-      text +="Olá, você recebeu um contato pelo CV online";
-      text +="<h2>";
-      text +="<p>"+nome+" escreveu a seguinte mensagem:</p>";
-      text +="<p>"+mensagem+"</p>";
+function formatMessage(nome, mensagem) {
+   var text = "";
+   text += "<h2>";
+   text += "Olá, você recebeu um contato pelo CV online";
+   text += "<h2>";
+   text += "<p>" + nome + " escreveu a seguinte mensagem:</p>";
+   text += "<p>" + mensagem + "</p>";
 
-      return text;
+   return text;
 }
 
 function sendMail() {
-      
-   
-    const nome = document.getElementById("nome").value;
-    const mensagem = document.getElementById("mensagem").value;
 
-    const texto = formatMessage(nome,mensagem);
 
-    const body = {
-        "personalizations":[
-           {
-              "to":[
-                 {
-                    "email":"gilvan.moura@outlook.com.br"
-                 }
-              ]
-           }
-        ],
-        "from":{
-           "email":"cadastros.ferrazdemoura@gmail.com"
-        },
-        "subject":"Contato do CV Online",
-        "content":[
-           {
-              "type":"text/html",
-              "value": texto
-           }
-        ]        
-     }
+   const nome = document.getElementById("nome").value;
+   const mensagem = document.getElementById("mensagem").value;
 
-    var myHeaders = new Headers({
-        'Content-Type': 'application/json',
-        'Authorization': `${secrets.SENDER}`
-    });
-        
-    var request = {
-        method: 'POST',
-        headers: myHeaders,
-        body: body        
-    };
+   const texto = formatMessage(nome, mensagem);
 
-    fetch('https://api.sendgrid.com/v3/mail/send', request)
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-    });
+   const body = {
+      "personalizations": [
+         {
+            "to": [
+               {
+                  "email": "gilvan.moura@outlook.com.br"
+               }
+            ]
+         }
+      ],
+      "from": {
+         "email": "cadastros.ferrazdemoura@gmail.com"
+      },
+      "subject": "Contato do CV Online",
+      "content": [
+         {
+            "type": "text/html",
+            "value": texto
+         }
+      ]
+   }
+
+   var myHeaders = new Headers({
+      'Content-Type': 'application/json',
+      'Authorization': `${secrets.SENDER}`
+   });
+
+   var request = {
+      method: 'POST',
+      headers: myHeaders,
+      body: body
+   };
+
+   fetch('https://api.sendgrid.com/v3/mail/send', request)
+      .then(function (response) {
+         console.log(response);
+      })
+      .catch(function (error) {
+         console.log(error);
+      });
 
 
 }
@@ -90,7 +90,7 @@ function sendMail() {
 //const env = require(["../env"]);
 
 function insertAfter(newElement, referenceElement) {
-    referenceElement.parentNode.insertBefore(newElement, referenceElement.nextSibling);
+   referenceElement.parentNode.insertBefore(newElement, referenceElement.nextSibling);
 }
 
 // if(pathName === "/"){
@@ -106,65 +106,65 @@ function insertAfter(newElement, referenceElement) {
 
 // }
 
-function formatMessage(nome, mensagem){
-      var text = "";
-      text +="<h2>";
-      text +="Olá, você recebeu um contato pelo CV online";
-      text +="<h2>";
-      text +="<p>"+nome+" escreveu a seguinte mensagem:</p>";
-      text +="<p>"+mensagem+"</p>";
+function formatMessage(nome, mensagem) {
+   var text = "";
+   text += "<h2>";
+   text += "Olá, você recebeu um contato pelo CV online";
+   text += "<h2>";
+   text += "<p>" + nome + " escreveu a seguinte mensagem:</p>";
+   text += "<p>" + mensagem + "</p>";
 
-      return text;
+   return text;
 }
 
 function sendMail() {
-      
-   
-    const nome = document.getElementById("nome").value;
-    const mensagem = document.getElementById("mensagem").value;
 
-    const texto = formatMessage(nome,mensagem);
 
-    const body = {
-        "personalizations":[
-           {
-              "to":[
-                 {
-                    "email":"gilvan.moura@outlook.com.br"
-                 }
-              ]
-           }
-        ],
-        "from":{
-           "email":"cadastros.ferrazdemoura@gmail.com"
-        },
-        "subject":"Contato do CV Online",
-        "content":[
-           {
-              "type":"text/html",
-              "value": texto
-           }
-        ]        
-     }
+   const nome = document.getElementById("nome").value;
+   const mensagem = document.getElementById("mensagem").value;
 
-    var myHeaders = new Headers({
-        'Content-Type': 'application/json',
-        'Authorization': `${secrets.SENDER}`
-    });
-        
-    var request = {
-        method: 'POST',
-        headers: myHeaders,
-        body: body        
-    };
+   const texto = formatMessage(nome, mensagem);
 
-    fetch('https://api.sendgrid.com/v3/mail/send', request)
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-    });
+   const body = {
+      "personalizations": [
+         {
+            "to": [
+               {
+                  "email": "gilvan.moura@outlook.com.br"
+               }
+            ]
+         }
+      ],
+      "from": {
+         "email": "cadastros.ferrazdemoura@gmail.com"
+      },
+      "subject": "Contato do CV Online",
+      "content": [
+         {
+            "type": "text/html",
+            "value": texto
+         }
+      ]
+   }
+
+   var myHeaders = new Headers({
+      'Content-Type': 'application/json',
+      'Authorization': `${secrets.SENDER}`
+   });
+
+   var request = {
+      method: 'POST',
+      headers: myHeaders,
+      body: body
+   };
+
+   fetch('https://api.sendgrid.com/v3/mail/send', request)
+      .then(function (response) {
+         console.log(response);
+      })
+      .catch(function (error) {
+         console.log(error);
+      });
 
 
 }
@@ -172,27 +172,26 @@ function sendMail() {
 //var button = document.getElementById("send");
 //button.addEventListener("click", sendMail, false);
 
-if(pathName=="/"){
+if (pathName == "/") {
    var els = document.getElementsByClassName("md-nav__item");
 
-var segundos = 2;
+   var segundos = 2;
 
-for(var i = 0; i < els.length; i++)
-{
-   //animate__animated animate__flipInX
-   els[i].classList.add("animate__animated");
-   els[i].classList.add("animate__flipInX");
-   els[i].style["animation-duration"] = segundos+"s";
-  
-   segundos ++;
+   for (var i = 0; i < els.length; i++) {
+      //animate__animated animate__flipInX
+      els[i].classList.add("animate__animated");
+      els[i].classList.add("animate__flipInX");
+      els[i].style["animation-duration"] = segundos + "s";
+
+      segundos++;
+   }
+
 }
 
-}
 
 
 
-
-// .classList.add("animate__animated"); 
+// .classList.add("animate__animated");
 // document.getElementsByClassName('.md-nav__item').classList.add("animate__flipInX"); 
 
 
