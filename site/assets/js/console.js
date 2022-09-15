872864ff0671370af2e7480e58f59a6fe0881199
gilvan <ti.gilvan@gmail.com>
2022-09-15 00:06:07 -0300

    Ajustes para produção

--- site/assets/js/console.js
+++
@@ -1,4 +0,0 @@
-console.log("\n%cBem vindo(a) ao meu currículo online\n%cAqui você encontra diversas informações sobre mim\n%cNavegue pelas diversas seções para saber mais sobre o meu trabalho!\n%c",
-"font-family: serif; font-size: 32px; color: #767676","font-family: serif; font-size: 25px; color: rgb(144, 39, 176); font-weight: bold;",
-"font-family: sans-serif; font-size: 16px; line-height: 1.2rem; color: #767676",
-"font-family: sans-serif; font-size: 11px; text-decoration: underline; line-height: 1.2rem; color: #767676");
